0be7e83493b81b5a286e6d4ae2823e02479b7a36
Mark <mark@tsoniq.com>
2018-08-01 19:52:00 +0200

    Initial Commit

---
+++ AudioFix/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  AudioFix
+//
+//  Created by Mark on 01/08/2018.
+//  Copyright © 2018 tSoniq. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
